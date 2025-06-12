5b82addaef8aef9aef67e1e7fd75681ec943babf
stoya <stoyanov03@mail.ru>
2025-06-13 02:03:09 +0300

    Stable back version

--- Server/core/models/Station.py
+++ Server/core/models/Station.py
@@ -1,6 +1,8 @@
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy import Column, String, Index
+from sqlalchemy import Column, String, Index, Boolean
 from geoalchemy2 import Geometry
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .base import Base
 
 
@@ -9,10 +11,9 @@ class Station(Base):
 
     name_station = Column(String)
     name_line = Column(String)
-    station_type = Column(String)
+    type: Mapped[str] = mapped_column(String(10), nullable=True)
     properties_data = Column(JSONB, nullable=True)
     geometry = Column(Geometry('POINT', srid=4326))
-
     __table_args__ = (
         Index('idx_stations_geom', geometry, postgresql_using='gist'),
     )
